731bd4dd1612c2129555fdf09ae012a8ae08348c
fatalc <cnfatal@gmail.com>
2021-11-01 12:45:48 +0800

    Initial Commit

---
+++ NetworkStatusBar/NetworkStatusBarApp.swift
@@ -0,0 +1,80 @@
+//
+//  NetworkStatusBarApp.swift
+//  NetworkStatusBar
+//
+//  Created by fatal cn on 2021/10/31.
+//
+
+import SwiftUI
+
+@main
+struct NetworkStatusBarApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appdelegate
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+var StatusBarWidth = CGFloat(60)
+
+class AppDelegate :NSObject, NSApplicationDelegate{
+
+    var iostates : IOStates = IOStates()
+    var statusItem : NSStatusItem?
+
+    var networkStatus :NetworkDetails = NetworkDetails()
+
+    func onUpdate(update:NetworkStates){
+        DispatchQueue.main.async {
+            self.iostates.total = update.Total
+            self.iostates.items = update.Items
+        }
+    }
+
+    func applicationWillFinishLaunching(_ notification: Notification) {
+        NSApp.setActivationPolicy(.prohibited)
+    }
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        statusItem?.length = StatusBarWidth
+
+        networkStatus.callback = onUpdate
+        DispatchQueue.global(qos: .userInteractive).async {
+            self.networkStatus.run(refreshSeconds: 1)
+        }
+
+        if let button = statusItem?.button{
+           let statusbarview = NSHostingView(rootView: StatusBarView(iostates: iostates ))
+           statusbarview.frame = button.frame
+           button.addSubview(statusbarview)
+        }
+
+        statusItem?.menu={
+            let menu = NSMenu()
+            menu.items = [
+                {
+                    let menuitem = NSMenuItem()
+                    menuitem.view = NSHostingView(rootView: StatusBarDetailsView(iostates: iostates))
+                    menuitem.view?.setFrameSize(NSSize(width: 210, height: 300))
+                    return menuitem
+                }(),
+                NSMenuItem(
+                    title: NSLocalizedString("quit", comment: "quit the application"),
+                    action: #selector(quit),
+                    keyEquivalent: "q"
+                )
+            ]
+            return menu
+        }()
+    }
+
+    @IBAction func quit(obj:Any){
+        NSApp.terminate(nil)
+    }
+}
+
+
+
---
+++ NetworkStatusBar/StatusBarDetailsView.swift
@@ -0,0 +1,55 @@
+//
+//  StatusBarDetailsView.swift
+//  NetworkStatusBar
+//
+//  Created by fatal cn on 2021/10/31.
+//
+
+import SwiftUI
+
+struct StatusBarDetailsView: View {
+    @ObservedObject var iostates = IOStates()
+    var body: some View {
+        List(iostates.items,id: \.pid){item in
+            StatusBarDetailsItemView(state: item)
+        }
+        .listStyle(.sidebar)
+    }
+}
+
+struct StatusBarDetailsItemView: View{
+    var state: NetworkState = NetworkState()
+
+    var body: some View{
+        HStack(alignment: VerticalAlignment.center, spacing: 0){
+            Text(state.name)
+                .frame(width:120,alignment: Alignment.leading)
+            VStack(alignment: .trailing){
+                Text(String(format: "%@ ▲", arguments: [formatbytes(state.outbounds)]) )
+                Text(String(format: "%@ ▼", arguments: [formatbytes(state.inbounds)]) )
+            }
+            .fixedSize()
+            .font(.system(size: 9,weight: .medium))
+            .frame(width: 65,alignment: Alignment.trailing)
+        }
+    }
+}
+
+struct StatusBarMenuView_Previews: PreviewProvider {
+    static let  data =  { () -> IOStates in
+        let ret  = IOStates()
+        ret.items = [
+            NetworkState(pid: 1, name: "process", inbounds: 512, outbounds: 512),
+            NetworkState(pid: 2, name: "process", inbounds: 64, outbounds: 1)]
+        return ret
+    }()
+    static var previews: some View {
+        StatusBarDetailsView(iostates: data )
+    }
+}
+
+struct StatusBarDetailsItemView_Previews: PreviewProvider{
+    static var previews: some View{
+        StatusBarDetailsItemView(state: NetworkState(name: "process", inbounds: 1023, outbounds: 1000))
+    }
+}
